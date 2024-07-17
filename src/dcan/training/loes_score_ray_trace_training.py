f803c998d4199fe29fc5d167d622d85d1cf16041
Paul Reiners <paul.reiners@gmail.com>
2024-07-17 12:27:27 -0500

    Came up with decent set of hyperparameters.

--- src/dcan/training/loes_score_ray_trace_training.py
+++ src/dcan/training/loes_score_ray_trace_training.py
@@ -207,10 +207,10 @@ def main(num_samples=10, max_num_epochs=10, gpus_per_trial=2):
     load_data()
 
     config = {
-        "lr": tune.loguniform(1e-4, 1e-1),
-        "batch_size": tune.choice([2, 4, 8, 16]),
-        "weight_decay": tune.loguniform(0.0001, 0.9990),
-        "dropout": tune.uniform(0.0000, 0.9990)
+        "lr": tune.loguniform(1e-4, 1e-3),
+        "batch_size": tune.choice([1, 2, 4]),
+        "weight_decay": tune.loguniform(1e-4, 1e-3),
+        "dropout": tune.uniform(0.005, 0.015)
     }
 
     scheduler = ASHAScheduler(
