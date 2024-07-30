83824bddbb79975eb888ef91f730fd0311630e39
Paul Reiners <paul.reiners@gmail.com>
2024-07-30 15:47:12 -0500

    Model 12

--- bin/loes-scoring-training_model09_mesabi.sh
+++ bin/loes-scoring-training_model09_mesabi.sh
@@ -16,11 +16,11 @@
 
 #SBATCH -A miran045
 
-cd /home/miran045/reine097/projects/loes-scoring-2/src/dcan/training || exit
-export PYTHONPATH=PYTHONPATH:"/home/miran045/reine097/projects/loes-scoring-2/src:/home/miran045/reine097/projects/AlexNet_Abrol2021/src"
+cd /home/miran045/reine097/projects2/loes-scoring-2/src/dcan/training || exit
+export PYTHONPATH=PYTHONPATH:"/home/miran045/reine097/projects2/loes-scoring-2/src:/home/miran045/reine097/projects/AlexNet_Abrol2021/src"
 /home/miran045/reine097/projects/AlexNet_Abrol2021/venv/bin/python \
-  /home/miran045/reine097/projects/loes-scoring-2/src/dcan/training/training.py \
-  --csv-data-file "/home/miran045/reine097/projects/loes-scoring-2/data/filtered/ashish_all.csv" \
+  /home/miran045/reine097/projects2/loes-scoring-2/src/dcan/training/training.py \
+  --csv-data-file "/home/miran045/reine097/projects2/loes-scoring-2/data/MNI-space_Loes_data.csv" \
   --batch-size 1 --num-workers 1 --epochs 256 \
   --model-save-location "/home/feczk001/shared/data/AlexNet/LoesScoring/loes_scoring_09.pt" \
   --output-csv-file "/home/miran045/reine097/projects/loes-scoring-2/doc/models/model09/output.csv" --gd 2 \
