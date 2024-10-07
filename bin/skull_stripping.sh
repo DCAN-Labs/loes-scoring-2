8458bd2afc2250c67b3635f55b67414e52950303
Paul Reiners <reine097@umn.edu>
2024-10-07 14:25:41 -0500

    Have skull stripping working now for hard-coded paths.

--- bin/skull_stripping.sh
+++ bin/skull_stripping.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-LOES_SCORING=/users/9/reine097/loes_scoring
-HEAD=${LOES_SCORING}/Loes_score/sub-1043EATO/ses-20190426/mprage.nii.gz
-DESKULLED_DIR=${LOES_SCORING}/deskulled
-SKULL_STRIPPED=${DESKULLED_DIR}/Loes_score/sub-1043EATO/ses-20190426/mprage.nii.gz
+# If you get an error, it may be because you don't have enough memory.
 
-cd $DESKULLED_DIR
+DIR=/home/feczk001/shared/projects/S1067_Loes/data/niftis_deID/atlas_reg
+HEAD=${DIR}/sub-01_session-01_space-MNI_mprage.nii.gz
+SKULL_STRIPPED=~/sub-01_session-01_space-MNI_mprage-deskulled.nii.gz
 
-mkdir -p Loes_score/sub-1043EATO/ses-20190426
+cd $DIR
 
 singularity run -B ${HEAD} /home/faird/shared/code/external/utilities/synthstrip_1.4.sif -i ${HEAD} -o ${SKULL_STRIPPED} --no-csf
