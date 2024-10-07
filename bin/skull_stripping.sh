3e8d2d093bd6ac1cdc6847fcc27b3031e82544d0
Paul Reiners <reine097@umn.edu>
2024-10-07 14:42:05 -0500

    Skull-stripping now takes args.

--- bin/skull_stripping.sh
+++ bin/skull_stripping.sh
@@ -2,10 +2,11 @@
 
 # If you get an error, it may be because you don't have enough memory.
 
-DIR=/home/feczk001/shared/projects/S1067_Loes/data/niftis_deID/atlas_reg
-HEAD=${DIR}/sub-01_session-01_space-MNI_mprage.nii.gz
-SKULL_STRIPPED=~/sub-01_session-01_space-MNI_mprage-deskulled.nii.gz
+HEAD_WITH_SKULL=${1}
+SKULL_STRIPPED_HEAD=${2}
 
 cd $DIR
 
-singularity run -B ${HEAD} /home/faird/shared/code/external/utilities/synthstrip_1.4.sif -i ${HEAD} -o ${SKULL_STRIPPED} --no-csf
+singularity run -B ${HEAD_WITH_SKULL} \
+    /home/faird/shared/code/external/utilities/synthstrip_1.4.sif \
+    -i ${HEAD_WITH_SKULL} -o ${SKULL_STRIPPED_HEAD} --no-csf
