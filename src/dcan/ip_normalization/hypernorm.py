aceed0e18110c1c781fdee657cbe9b931245387b
Paul Reiners <reine097@umn.edu>
2024-10-08 12:52:34 -0500

    Starting work on normalization.

---
+++ src/dcan/ip_normalization/hypernorm.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+
+def normalize_file(orig_anat, out, tx):
+    os.chdir('/home/feczk001/shared/projects/nnunet_predict/task550_testing/hypernorm/')
+
+    if tx=='T1':
+        template='templates/0mo_T1_average.nii.gz'
+    else:
+        template='templates/0mo_T2_average.nii.gz'
+
+    cmd='fslmaths {} -sub `fslstats {} -M` -div `fslstats {} -S` -mul `fslstats {} -S` -add `fslstats {} -M` {}'.format(orig_anat,orig_anat,orig_anat,template,template,out)
+    os.system(cmd)
+
+if __name__ == '__main__':
+    orig_anat = sys.argv[1]
+    out = sys.argv[2]
+    tx = sys.argv[3]
+    normalize_file(orig_anat, out, tx)
