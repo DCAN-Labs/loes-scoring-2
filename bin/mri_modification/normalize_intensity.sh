8797fe4fbb62525971d24891389b6ab7bbfff072
Paul Reiners <reine097@umn.edu>
2024-10-09 14:27:34 -0500

    Adding normalize intensity script.

---
+++ bin/mri_modification/normalize_intensity.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+module load fsl
+
+SUBJECT_BRAIN=$1
+MNI_TEMPLATE_BRAIN=$2
+SUBJECT_BRAIN_NORMALIZED=$3
+
+echo Normalizing ${SUBJECT_BRAIN}
+cmd="fslmaths ${SUBJECT_BRAIN} -sub `fslstats ${SUBJECT_BRAIN} -M` -div `fslstats ${SUBJECT_BRAIN} -S` -mul `fslstats ${MNI_TEMPLATE_BRAIN} -S` -add `fslstats $ -M` ${SUBJECT_BRAIN_NORMALIZED}"
+echo $cmd
+$cmd
