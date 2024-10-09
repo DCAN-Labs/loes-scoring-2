3ed12fa71e1d2b1fda73a226855e85c794fe3b50
Paul Reiners <reine097@umn.edu>
2024-10-09 15:21:58 -0500

    Added missing argument.

--- bin/mri_modification/normalize_intensity.sh
+++ bin/mri_modification/normalize_intensity.sh
@@ -7,6 +7,6 @@ MNI_TEMPLATE_BRAIN=$2
 SUBJECT_BRAIN_NORMALIZED=$3
 
 echo Normalizing ${SUBJECT_BRAIN}
-cmd="fslmaths ${SUBJECT_BRAIN} -sub `fslstats ${SUBJECT_BRAIN} -M` -div `fslstats ${SUBJECT_BRAIN} -S` -mul `fslstats ${MNI_TEMPLATE_BRAIN} -S` -add `fslstats $ -M` ${SUBJECT_BRAIN_NORMALIZED}"
+cmd="fslmaths ${SUBJECT_BRAIN} -sub `fslstats ${SUBJECT_BRAIN} -M` -div `fslstats ${SUBJECT_BRAIN} -S` -mul `fslstats ${MNI_TEMPLATE_BRAIN} -S` -add `fslstats ${MNI_TEMPLATE_BRAIN} -M` ${SUBJECT_BRAIN_NORMALIZED}"
 echo $cmd
 $cmd
