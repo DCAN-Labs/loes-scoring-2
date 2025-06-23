7043bb1dd1a580411b892310fa2f89f4397f703d
Paul Reiners <reine097@umn.edu>
2025-06-23 08:56:54 -0500

    Cleaned up various things.

--- scripts/training/regression/loes-scoring-training_model_agate_20.sh
+++ scripts/training/regression/loes-scoring-training_model_agate_20.sh
@@ -12,14 +12,12 @@
 #SBATCH -e loes-scoring-alex-net-%j.err
 #SBATCH -o loes-scoring-alex-net-%j.out
 
-#256
-
 cd /users/9/reine097/projects/loes-scoring-2/src/dcan/training || exit
 export PYTHONPATH=PYTHONPATH:"/users/9/reine097/projects/loes-scoring-2/src:/users/9/reine097/projects/AlexNet_Abrol2021/src/"
 /users/9/reine097/projects/loes-scoring-2/.venv/bin/python \
   /users/9/reine097/projects/loes-scoring-2/src/dcan/regression/training.py \
   --csv-input-file "/users/9/reine097/projects/loes-scoring-2/data/anon_train_scans_and_loes_training_test_non_gd.csv" \
-                        --batch-size 1 --num-workers 1 --epochs 64 \
+                        --batch-size 1 --num-workers 1 --epochs 256 \
                         --model-save-location "/home/feczk001/shared/data/LoesScoring/loes_scoring_20.pt" \
                         --gd  0 \
                         --folder "/home/feczk001/shared/projects/S1067_Loes/data/Fairview-ag/05-training_ready/" \
